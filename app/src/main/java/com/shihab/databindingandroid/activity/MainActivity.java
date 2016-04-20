1335c06f3ebe7a2916f3295b15d3b4f2b9acf4b1
shihabmi7 <shihab.mi7@gmail.com>
2016-04-20 15:54:41 +0600

    // Use Better knife with data binding

--- app/src/main/java/com/shihab/databindingandroid/activity/MainActivity.java
+++ app/src/main/java/com/shihab/databindingandroid/activity/MainActivity.java
@@ -5,15 +5,34 @@ import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.shihab.databindingandroid.R;
 import com.shihab.databindingandroid.databinding.ActivityMainBinding;
 import com.shihab.databindingandroid.model.User;
 
-//
+import butterknife.Bind;
+import butterknife.BindString;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+/*
 
+// How to use better knife with Jake Warton :)
+
+*/
 public class MainActivity extends AppCompatActivity {
 
+
+    @Bind(R.id.textview_test)
+    TextView textView;
+
+    @BindString(R.string.app_name)
+    String mes;
+
+    @OnClick(R.id.button_clickMe) void Click(){
+
+        Toast.makeText(getApplicationContext(),mes +"with better knife" ,Toast.LENGTH_LONG).show();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,19 +42,18 @@ public class MainActivity extends AppCompatActivity {
         User user = new User("First Name: Shihab", "Last Name: Uddin");
         binding.setUser(user);
 
+        ButterKnife.bind(this);
 
     }
 
     @Override
     public void onStart() {
         super.onStart();
-
     }
 
     @Override
     public void onStop() {
         super.onStop();
-
 
     }
 }
